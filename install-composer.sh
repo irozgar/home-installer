931ce6a7d706789bb7c3439c7348c0f03aacea22
irozgar <me@irozgar.eu>
2014-10-25 17:02:00 +0200

    Composer installer: Check if PHP is installed before trying to install

--- install-composer.sh
+++ install-composer.sh
@@ -6,6 +6,9 @@ installDir="$HOME/.local/bin/"
 
 pushd $installDir
 
+# Check if php is installed
+type php > /dev/null 2>&1 || { echo >&2 "Composer requires PHP but it's not installed. Abort."; popd; exit 1; }
+
 curl -Ss http://getcomposer.org/installer | php
 
 popd
